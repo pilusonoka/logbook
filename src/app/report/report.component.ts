cb14dba76a87f2f1bc18011369f028f20e41212b
karina <k.pilyushonoka@gmail.com>
2019-05-29 03:41:47 +0300

    Init table for report

--- src/app/report/report.component.ts
+++ src/app/report/report.component.ts
@@ -1,8 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { ApplicationStateService } from '../services/application-state.service';
+import { StorageService } from '../services/storage.service';
+import { Flight } from '../models/flight';
+import { Pilot } from '../models/pilot';
 
 @Component({
   selector: 'lg-report',
   template: `
+  <table mat-table [dataSource]="flights" class="mat-elevation-z8">
+    <ng-container matColumnDef="date">
+      <th mat-header-cell *matHeaderCellDef> Date </th>
+      <td mat-cell *matCellDef="let element"> {{element.date | date}} </td>
+    </ng-container>
+
+    <ng-container matColumnDef="aircraft">
+      <th mat-header-cell *matHeaderCellDef> Aircraft </th>
+      <td mat-cell *matCellDef="let element"> {{element.aircraft}} </td>
+    </ng-container>
+
+    <ng-container matColumnDef="duration">
+      <th mat-header-cell *matHeaderCellDef> duration </th>
+      <td mat-cell *matCellDef="let element"> {{element.duration | hours}} </td>
+    </ng-container>
+
+    <tr mat-header-row *matHeaderRowDef="displayedColumns"></tr>
+    <tr mat-row *matRowDef="let row; columns: displayedColumns;"></tr>
+  </table>
     <p>
       report works!
     </p>
@@ -10,10 +33,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./report.component.scss']
 })
 export class ReportComponent implements OnInit {
+  flights: Flight[];
+  pilot: Pilot;
+  displayedColumns: string[];
+  dataSource: Flight[];
 
-  constructor() { }
+  constructor(
+    private applicationState: ApplicationStateService,
+    private storageService: StorageService) { }
 
   ngOnInit() {
+    this.pilot = this.applicationState.pilot;
+    this.storageService
+        .getAllFlights(this.pilot)
+        .then(res => this.flights = res)
+        .then(res => {
+          this.displayedColumns = ['date', 'aircraft', 'duration'];
+          this.dataSource = this.flights;
+        });
   }
 
 }
--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -20,13 +20,29 @@ export class StorageService {
 
   getUserInfo(uid: string): Promise<any> {
     return this.db.collection('users')
-               .doc(uid).ref
+               .doc(uid)
+               .ref
                .get()
                .then(doc => doc.exists? doc.data() : null);
   }
 
-  getAllFlights(pilot: Pilot): Flight[] {
-    return null;
+  getAllFlights(pilot: Pilot): Promise<Flight[]> {
+    return this.db.collection('flights')
+               .ref
+               .where('pilotId', '==', pilot.uid)
+               .orderBy('date', 'desc')
+               .get()
+               .then((res) => {
+                  const flights: Flight[] = [];
+                  res.forEach(doc => {
+                    if (doc.exists) {
+                     const flightDTO = doc.data();
+                     const flight = Object.assign({}, flightDTO, {date: flightDTO.date.toDate()}) as Flight;
+                     flights.push(flight);
+                    }
+                  });
+                  return flights;
+               });
   }
 
   saveNewFlight(flight: Flight): Promise<any> {
